0377a5ef8ababfac9f8d756e554a7c3e9b90c409
archomai0191 <90777703+archomai0191@users.noreply.github.com>
2023-05-15 00:51:09 +0500

    Analysis modules

---
+++ SnowMeltAnalysis.py
@@ -0,0 +1,182 @@
+import tkinter as tk, os, pandas as pd, datetime as dt
+from tkinter import filedialog as fd, messagebox as msg
+from tkinter.messagebox import showinfo
+from pathlib import Path
+from simpledbf import Dbf5
+
+def loadRealData():
+    global realDataFile
+    realDataFile = fd.askopenfilename(title = "Выберите файл с данными снегомерных маршрутов", \
+        filetypes=[('Файлы XLSX', '*.xlsx')], initialdir = app_folder)
+
+def directory_selector(windowTitle: str, selected: list):
+    directory_path_string = fd.askdirectory(initialdir=app_folder, title=windowTitle)
+
+    if len(directory_path_string) > 0:
+        selected.append(directory_path_string)
+        print ("Выбранные папки: ", selected)
+        directory_selector('Выберите следующую папку или нажмите Отмена, чтобы закончить', selected)
+
+    return selected
+
+def calcError(folder, year, result_type):
+    global realDataFile
+    result_path = f"{folder}/{result_type}_group.dbf"
+    result_frame = Dbf5(result_path).to_dataframe()
+    real_frame = pd.read_excel(realDataFile)
+    group_like = real_frame.columns.values[0]
+    checkout_group = result_frame[(result_frame["GROUP_ID"].str.startswith(group_like))]
+
+    sumReal = sumCalced = routeDays = error = 0
+    for i in range(2,real_frame.shape[0]):
+        row = real_frame.loc[[i]]
+        date = pd.to_datetime(row[group_like], format='%Y-%m-%d %H:%M:%S')
+        if (date.dt.year == int(year)).all():
+            sumReal += row[2].values[0]
+            calced = checkout_group[(pd.to_datetime(checkout_group["DATE"], format='%d.%m.%Y').dt.strftime('%d.%m.%Y') == pd.Timestamp(date.values[0]).to_pydatetime().strftime('%d.%m.%Y'))]["DS"].mean()
+            sumCalced += calced
+            error += (row[2].values[0] - calced) / row[2].values[0]
+            routeDays += 1
+        elif (date.dt.year > int(year)).all(): break
+    
+    error = error / routeDays
+    return (sumReal, sumCalced, routeDays, error)
+
+def calcMeltInfo(folder, result_type):
+    global realDataFile
+    result_path = f"{folder}/{result_type}_group.dbf"
+    result_frame = Dbf5(result_path).to_dataframe()
+    group_list = result_frame["GROUP_ID"].unique().tolist()
+
+    has_group = result_frame
+    no_group = pd.DataFrame()
+    minstart_group = ""
+    minend_group = ""
+    maxstart_group = ""
+    maxend_group = ""
+    minstart_date = dt.datetime.now()
+    minend_date = dt.datetime.now()
+    maxstart_date = dt.datetime.now()
+    maxend_date = dt.datetime.now()
+    for i in range(0, len(group_list)):
+        if i < len(group_list) - 1: no_group, has_group = [x for _, x in \
+            has_group.groupby(has_group["GROUP_ID"] == group_list[i])]
+
+        filtered = has_group[(has_group["H"] > 0)]
+        if len(filtered) == 0:
+            has_group = no_group
+            continue
+        min_row = filtered.iloc[0]
+        start = pd.to_datetime(min_row["DATE"], format='%d.%m.%Y')
+        max_row = filtered.iloc[-1]
+        end = pd.to_datetime(max_row["DATE"], format='%d.%m.%Y')
+        minstart_date = start if minstart_group == "" or start < minstart_date else minstart_date
+        minend_date = end if minend_group == "" or end < minend_date else minend_date
+        maxstart_date = start if maxstart_group == "" or start > maxstart_date else maxstart_date
+        maxend_date = end if maxend_group == "" or end > maxend_date else maxend_date
+
+        minstart_group = group_list[i] if minstart_date == start else minstart_group
+        minend_group = group_list[i] if minend_date == end else minend_group
+        maxstart_group = group_list[i] if maxstart_date == start else maxstart_group
+        maxend_group = group_list[i] if maxend_date == end else maxend_group
+
+        has_group = no_group
+    return minstart_date, minstart_group, maxstart_group, minend_group, maxend_group, maxend_date
+
+def loadData(folder, result_type):
+    global dFrame
+    if not Path(folder + "/results_" + result_type).exists:
+        raise Exception (f"В папке нет данных по вариации коэффициента (путь к папке: {folder})")
+    
+    year = os.path.basename(folder)
+    for subFolder in os.listdir(folder + "/results_" + result_type):
+        try:
+            convRes = float(subFolder.replace(",", "."))
+        except Exception: continue
+        subFolderFull = folder + "/results_" + result_type + "/" + subFolder
+        if os.path.isdir(subFolderFull):
+            if not Path(subFolderFull + "/les_group.dbf").exists and not Path(subFolderFull + "/pole_group.dbf").exists:
+                raise Exception(
+                f"Для одного из коэффициентов нет данных расчетов по лесу и/или полю (путь к папке: {subFolderFull})"
+                )
+            coef = float(os.path.basename(subFolder).replace(",", "."))
+            result_typeMorph = "les" if result_type == "forest" else "pole"
+            sumReal, sumCalced, routeDays, error = calcError(subFolderFull, year, \
+                result_typeMorph)
+            minstart_date, minstart_group, maxstart_group, minend_group, maxend_group, maxend_date = \
+                calcMeltInfo(subFolderFull, result_typeMorph)
+            row = {"year": [year], "coef": [coef], "coefStr": [str(coef).replace(".", ",")], "sumReal": [sumReal], "sumCalced": [sumCalced], \
+                "routeDays": [routeDays], "error": [error], "errorAbs": [abs(error)], "minStartDate": minstart_date, \
+                "minStartGroup": minstart_group, "maxStartGroup": maxstart_group, "minEndGroup": minend_group, \
+                "maxEndGroup": maxend_group, "maxEndDate": maxend_date}
+            rowFrame = pd.DataFrame(row)
+            dFrame = pd.concat([dFrame, rowFrame], axis = 0, ignore_index=True)
+
+def collectDataset():
+    global dFrame
+    oldFrame = dFrame
+
+    try:
+        if realDataFile == "": loadRealData()
+        if realDataFile == "": raise Exception("Сначала выберите файл с данными снегомерных маршрутов")
+        foldersInit = []
+        folders = directory_selector("Выберите папки с данными расчетов", foldersInit)
+        result_type = msg.askyesno(title="Выберите тип ландшафта", \
+            message="Будут проводиться расчеты для леса?")
+        result_type = "forest" if result_type else "field"
+        print("Итоговый выбор: ", folders)
+        dFrame = pd.DataFrame(columns=["year", "coefStr", "sumReal", "sumCalced", "routeDays", "error", "errorAbs",\
+            "minStartDate", "minStartGroup", "maxStartGroup", "minEndGroup", "maxEndGroup", "maxEndDate", "coef"])
+
+        for folder in folders:
+            try: loadData(folder, result_type)
+            except Exception as e:
+                showinfo(title="Ошибка агрегации", message=str(e))
+                continue
+            finally:
+                print(f"{folder} done")
+
+        print(dFrame)
+        if (dFrame.size == 0): raise Exception("Ничего импортировать не удалось")
+        else:
+            saveFile = fd.asksaveasfilename(title="Выберите, куда сохранить датасет", \
+                filetypes=[('Файлы CSV', '*.csv')])
+            dFrame.to_csv(saveFile)
+
+    except Exception as e:
+        showinfo(title="Ошибка агрегации", message=str(e))
+        dFrame = oldFrame
+
+
+app_folder = os.path.dirname(os.path.realpath(__file__))
+app_folder = app_folder.replace("\\", "/")
+realDataFile = ""
+dFrame = pd.DataFrame()
+os.system("chcp 65001 > nul")
+
+form = tk.Tk() #создание окна
+#параметры окна
+form.title("Расчет снеготаяния")
+form.minsize(400, 300)
+form.resizable(False, False)
+
+lbl = tk.Label(form)
+lbl.grid(row = 0, column = 0)
+lbl1 = tk.Label(form)
+lbl1.grid(row = 1, column = 0)
+lbl2 = tk.Label(form)
+lbl2.grid(row = 2, column = 0)
+lbl3 = tk.Label(form)
+lbl3.grid(row = 3, column = 0)
+
+loadReal = tk.Button(form, text="Выбрать файл с данными снегомерных маршрутов", width=100, \
+    command=loadRealData)
+loadReal.grid(row = 4, column = 0)
+
+lbl4 = tk.Label(form)
+lbl4.grid(row = 5, column = 0)
+
+collectData = tk.Button(form, text="Собрать данные расчетов в один датасет", width=100,\
+    command=collectDataset)
+collectData.grid(row = 6, column = 0)
+form.mainloop()
---
+++ SnowMeltSingle.py
@@ -0,0 +1,184 @@
+#Модуль, содержащий описание формы "одиночного" расчета
+
+import tkinter as tk, FormDataClass as fdc, math, datetime as dt, SnowMeltLogic as sml
+from tkinter import filedialog as fd
+from tkinter.ttk import Checkbutton
+from tkinter.messagebox import showinfo
+
+def heightClicked(): #обработчик для checkbox-a "Высота"
+    forestCoefEntry.configure(state = "normal" if heightChk_state.get() else "disabled")
+    fieldCoefEntry.configure(state = "normal" if heightChk_state.get() else "disabled")
+
+def expClicked(): #обработчик для checkbox-a "Экспозиция"
+    southEntry.configure(state = "normal" if expChk_state.get() else "disabled")
+    westEntry.configure(state = "normal" if expChk_state.get() else "disabled")
+    eastEntry.configure(state = "normal" if expChk_state.get() else "disabled")
+    northEntry.configure(state = "normal" if expChk_state.get() else "disabled")
+    plainEntry.configure(state = "normal" if expChk_state.get() else "disabled")
+
+def forestClicked(): #обработчик для checkbox-a "Изменить водный эквивалент снега для леса"
+    snowForestDefEntry.set("2")
+    snowForestCoefEntry.configure(state = "normal" if snowForestChk_state.get() else "disabled")
+
+def fieldClicked(): #обработчик для checkbox-a "Изменить водный эквивалент снега для поля"
+    snowFieldDefEntry.set("5")
+    snowFieldCoefEntry.configure(state = "normal" if snowFieldChk_state.get() else "disabled")
+
+def pickFile(): #обработчик для кнопки "Выбрать файл"
+    filetypes = [('Файлы DBF', '*.dbf')]
+    filename = fd.askopenfilename(title = "Выберите файл", initialdir = "/", filetypes = filetypes)
+    pathLbl.configure(text = filename if filename != "" else "Файл с данными о точках не выбран")
+
+def parseEntry(elem, message): #преобразование строки в вещественное число
+    try:
+        res = float(elem.get().replace(",", ".")) #замена запятой на точку и преобразование к вещественному числу
+        return res
+    except: #преобразовать не удалось => было введено не число, сообщаем об этом
+        showinfo(title = "Ошибка в параметрах", message = message)
+        return float('NaN')
+
+def run(): #обработчик для кнопки "Расчет"
+    #чтение и проверка временного промежутка
+    startTime = sml.parseDt(startEntry, True, "Неверный формат для начала временного промежутка")
+    endTime = sml.parseDt(endEntry, False, "Неверный формат для конца временного промежутка")
+    if startTime is None or endTime is None: return
+    if startTime > endTime:
+        showinfo(title = "Ошибка в параметрах", message = 
+                 "Начало временного промежутка не может наступить позже, чем его конец")
+        return
+
+    #чтение и проверка порога высоты и коэффициентов водного эквивалента снега
+    heightTh = parseEntry(heightEntry, "В качестве порога высоты введено не число")
+    wEqField = parseEntry(snowFieldCoefEntry, "В качестве водного эквивалента снега для поля введено не число")
+    wEqForest = parseEntry(snowForestCoefEntry, "В качестве водного эквивалента снега для леса введено не число")
+    if math.isnan(heightTh) or math.isnan(wEqField) or math.isnan(wEqForest): return
+
+    if not heightChk_state.get(): #если высота не учитывается, высотные коэффициенты имеют значения по умолчанию
+        forestCoef = 0.9739
+        fieldCoef = 0.9739
+    else: #иначе - проверка этих коэффициентов
+        forestCoef = parseEntry(forestCoefEntry, "В качестве высотного коэффициента для леса введено не число")
+        fieldCoef = parseEntry(fieldCoefEntry, "В качестве высотного коэффициента для поля введено не число")
+        if math.isnan(forestCoef) or math.isnan(fieldCoef): return
+    
+    if not expChk_state.get(): #если экспозиция не учитывается, экспозиционные коэффициенты имеют значения по умолчанию
+        sCoef = 1
+        nCoef = 1
+        wCoef = 1
+        eCoef =  1
+        pCoef = 1
+    else: #иначе - проверка этих коэффициентов
+        sCoef = parseEntry(southEntry, "В качестве экспозиционного коэффициента для юга введено не число")
+        nCoef = parseEntry(northEntry, "В качестве экспозиционного коэффициента для севера введено не число")
+        wCoef = parseEntry(westEntry, "В качестве экспозиционного коэффициента для запада введено не число")
+        eCoef = parseEntry(eastEntry, "В качестве экспозиционного коэффициента для востока введено не число")
+        pCoef = parseEntry(plainEntry, "В качестве экспозиционного коэффициента для равнины введено не число")
+        if math.isnan(sCoef) or math.isnan(nCoef) or math.isnan(wCoef) or math.isnan(eCoef) or math.isnan(pCoef): return
+
+    if pathLbl.cget("text") == "Файл с данными о точках не выбран":
+        showinfo(title = "Ошибка в параметрах", message = "Сначала выберите файл")
+        return
+
+    #запуск расчетов
+    data = fdc.FormData(pathLbl.cget("text"), startTime, endTime, heightTh, heightChk_state.get(), expChk_state.get(),
+                       wEqField, wEqForest, forestCoef, fieldCoef, sCoef, nCoef, wCoef, eCoef, pCoef)
+    try:
+        sml.run(data)
+        showinfo(title = "Данные успешно сохранены", message="Вы можете просмотреть файлы")
+    except Exception as e:
+        showinfo(title = "Ошибка при расчетах", message = str(e))
+
+form = tk.Tk() #создание окна
+#параметры окна
+form.title("Расчет снеготаяния")
+form.minsize(400, 400)
+form.resizable(False, False)
+
+#временной промежуток, для которого ведутся расчеты
+startLbl = tk.Label(form, text = "Период с")
+startLbl.grid(column = 0, row = 0, sticky = "W")
+startEntry = tk.Entry(form)
+startEntry.grid(column = 1, row = 0, padx = 10)
+endLbl = tk.Label(form, text = "по")
+endLbl.grid(column = 2, row = 0)
+endEntry = tk.Entry(form)
+endEntry.grid(column = 3, row = 0, padx = 10)
+
+#порог высоты
+heightLbl = tk.Label(form, text = "Порог высоты")
+heightLbl.grid(column = 0, row = 1, pady = 10, sticky = "W")
+heightEntry = tk.Entry(form, width = 10)
+heightEntry.grid(column = 1, row = 1)
+
+#высота
+heightChk_state = tk.BooleanVar()
+heightChk_state.set(True)
+heightChk = Checkbutton(form, text = "Высота", var = heightChk_state, command = heightClicked)
+heightChk.grid(column = 0, row = 3, pady = 10, sticky = "W")
+forestCoefLbl = tk.Label(form, text = "Коэф. для леса")
+forestCoefLbl.grid(column = 1, row = 3)
+forestCoefEntry = tk.Entry(form, width = 10)
+forestCoefEntry.grid(column = 2, row = 3)
+fieldCoefLbl = tk.Label(form, text = "Коэф. для поля")
+fieldCoefLbl.grid(column = 1, row = 4)
+fieldCoefEntry = tk.Entry(form, width = 10)
+fieldCoefEntry.grid(column = 2, row = 4)
+
+#экспозиция
+expChk_state = tk.BooleanVar()
+expChk_state.set(True)
+expChk = Checkbutton(form, text = "Экспозиция", var = expChk_state, command = expClicked)
+expChk.grid(column = 0, row = 5, pady = 10, sticky = "W")
+southLbl = tk.Label(form, text = "Юг")
+southLbl.grid(column = 1, row = 5)
+southEntry = tk.Entry(form, width = 10)
+southEntry.grid(column = 2, row = 5)
+westLbl = tk.Label(form, text = "Запад")
+westLbl.grid(column = 1, row = 6)
+westEntry = tk.Entry(form, width = 10)
+westEntry.grid(column = 2, row = 6)
+northLbl = tk.Label(form, text = "Север")
+northLbl.grid(column = 1, row = 7, pady = 10)
+northEntry = tk.Entry(form, width = 10)
+northEntry.grid(column = 2, row = 7)
+eastLbl = tk.Label(form, text = "Восток")
+eastLbl.grid(column = 1, row = 8)
+eastEntry = tk.Entry(form, width = 10)
+eastEntry.grid(column = 2, row = 8)
+plainLbl = tk.Label(form, text = "Равнина")
+plainLbl.grid(column = 1, row = 9, pady = 10)
+plainEntry = tk.Entry(form, width = 10)
+plainEntry.grid(column = 2, row = 9)
+
+#коэффициенты водного эквивалента снега
+snowForestChk_state = tk.BooleanVar()
+snowForestChk_state.set(True)
+snowForestChk = Checkbutton(form, text = "Изменить водный эквивалент снега для леса", var = snowForestChk_state,
+                            command = forestClicked)
+snowForestChk.grid(column = 0, row = 10, sticky = "W")
+snowForestDefEntry = tk.StringVar()
+snowForestDefEntry.set("2")
+snowForestCoefEntry = tk.Entry(form, width = 10, textvariable = snowForestDefEntry)
+snowForestCoefEntry.grid(column = 2, row = 10)
+snowFieldChk_state = tk.BooleanVar()
+snowFieldChk_state.set(True)
+snowFieldChk = Checkbutton(form, text = "Изменить водный эквивалент снега для поля", var = snowFieldChk_state,
+                           command = fieldClicked)
+snowFieldChk.grid(column = 0, row = 11, pady = 10, sticky = "W")
+snowFieldDefEntry = tk.StringVar()
+snowFieldDefEntry.set("5")
+snowFieldCoefEntry = tk.Entry(form, width = 10, textvariable = snowFieldDefEntry)
+snowFieldCoefEntry.grid(column = 2, row = 11)
+
+#путь к файлу с данными о точках
+pathLbl = tk.Label(form, text = "Файл с данными о точках не выбран")
+pathLbl.grid(column = 0, row = 12, sticky = "W")
+
+#выбор файла и начало расчетов
+fileBtn = tk.Button(form, text = "Выбрать файл", width = 20, command = pickFile)
+fileBtn.grid(column = 0, row = 13, padx = 20, pady = 10)
+runBtn = tk.Button(form, text = "Расчет", width = 20, command = run)
+runBtn.grid(column = 1, row = 13)
+
+#отображение окна и запуск приложения
+form.mainloop()
